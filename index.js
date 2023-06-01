8865d83eaa1c0004c985d4665096751f272a3479
gmoliva <gmo.olivap@gmail.com>
2023-05-31 20:30:33 -0400

    added env to test docker copy

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 const router = express.Router();
+dotenv.config()
+
+PRINT_THIS_SHIT = process.env.hola
 
 app.use(express.json());
 
@@ -13,7 +17,10 @@ app.use("/general", router.get("/test", (req, res) => {
 const PORT = 3000;
 
 try {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}
+    \n
+    ${PRINT_THIS_SHIT}
+    `));
 
 } catch {((error) => console.log("error starting app"));}
     
